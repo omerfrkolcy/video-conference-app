4a24e6242347c51e6d769a2a5b4a8f1262e90b38
omerfrkolcy <omerfrkolcy@hotmail.com>
2023-11-29 05:30:21 +0300

    updating storybook components

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: ['@storybook/addon-links', {
     name: '@storybook/addon-essentials',
     options: {
@@ -7,12 +8,18 @@ module.exports = {
       docs: false
     }
   }, '@storybook/preset-create-react-app'],
-  framework: '@storybook/react',
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+
   webpackFinal: config => {
     config.resolve.alias['twilio-video'] = require.resolve('../src/stories/mocks/twilio-video.js');
     return config;
   },
-  core: {
-    builder: 'webpack5'
+
+  docs: {
+    autodocs: true
   }
 };
